add189f7664061cab35e42527753203a6e41ac0a
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-11-06 10:20:33 +0900

    display input form

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log('Hello TS!');
+var ProjectInput = /** @class */ (function () {
+    function ProjectInput() {
+        this.templateElement = document.getElementById("project-input");
+        this.hostElement = document.getElementById("app");
+        var importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.attach();
+    }
+    ProjectInput.prototype.attach = function () {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    };
+    return ProjectInput;
+}());
+var prjInput = new ProjectInput();
